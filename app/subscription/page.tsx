c5c8925bc40cc15a3e3467b2494fa1f4df8ca58b
Janielson Anjos <jan.tablet.uni@gmail.com>
2026-01-16 16:36:11 -0300

    chore: add Tailwind CSS configuration and integrate Prettier for code formatting

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,9 +1,9 @@
 const Subscription = () => {
-    return (
-        <div>
-            <h1>Subscription Page</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Subscription Page</h1>
+    </div>
+  );
+};
 
 export default Subscription;
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,9 +1,9 @@
 const Transactions = () => {
-    return (
-        <div>
-            <h1>Transactions Page</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Transactions Page</h1>
+    </div>
+  );
+};
 
 export default Transactions;
